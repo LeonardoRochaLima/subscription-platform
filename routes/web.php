19bda044d65516469cce1634831c40115b14c5e9
Leonardo Lima <leonardo.lima@edu.unifil.br>
2021-10-16 14:19:41 -0300

    Creating routes of Post

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,13 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+//Post Routes
+Route::resource('posts', 'PostController');
+Route::get('posts', 'PostController@index')->name('posts.index');
+Route::post('/posts/create', 'PostController@store');
+Route::post('posts/{post}/update', 'PostController@update');
+Route::post('/posts/{post}/deletePost', 'PostController@destroy');
+Route::post('/cadastros/{empresa}/excluirCadastro', 'EmpresaController@excluirCadastro')->name('cadastroEmpresa.excluirCadastro');
+
+//Site Routes
